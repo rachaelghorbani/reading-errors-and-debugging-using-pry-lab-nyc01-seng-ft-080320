306a000be5120210494f3b5f33dbfee80e475015
Rachael Ghorbani <rachael.ghorbani@gmail.com>
2020-07-10 17:05:08 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -8,10 +8,12 @@ end
 def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
-binding.pry
+
 def engage
   puts state_log(date)
   date = generate_star_date
+  binding.pry
+
 end
 
 =begin
