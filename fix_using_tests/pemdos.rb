0e11d730501c5761aa2cfd938616ae17beaebd70
Rachael Ghorbani <rachael.ghorbani@gmail.com>
2020-07-10 16:50:07 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -5,11 +5,10 @@ def snake_it_up(string)
   if string[0] == "s"
     10 * "s" + string
   else
-    binding.pry
     string
   end
 end
-
+    binding.pry
 
 =begin describe "pemdos" do
   it "`snake_it_up` adds the letter 's' ten times before any word that starts with 's'" do
