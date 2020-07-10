efc91c723dd5a51986d671600418b53de4e3167a
Rachael Ghorbani <rachael.ghorbani@gmail.com>
2020-07-10 16:55:37 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -4,11 +4,12 @@ require 'pry'
 def snake_it_up(string)
 
   if string[0] == "s"
-        binding.pry
 
     10.times do
       "s" + string
     end
+            binding.pry
+
   else
     string
   end
