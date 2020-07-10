80b976f98cd129b4fc734ea53b6dd816a32dca0e
Rachael Ghorbani <rachael.ghorbani@gmail.com>
2020-07-10 17:01:51 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -4,7 +4,7 @@ require 'pry'
 def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
-
+binding.pry
 
 def state_log(star_date)
   "Captain's Log, star date #{star_date}."
@@ -14,3 +14,11 @@ def engage
   puts state_log(date)
   date = generate_star_date
 end
+
+=begin
+describe "the_final_frontier" do
+  it "`engage` should return the captain's log and crew greetings" do
+    expect{ engage }.to output(/Captain's Log, star date /).to_stdout
+  end
+end
+=end
