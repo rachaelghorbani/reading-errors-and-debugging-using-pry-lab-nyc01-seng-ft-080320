8e33a76c4be7dd248b99a147b5207134af50a7c5
Rachael Ghorbani <rachael.ghorbani@gmail.com>
2020-07-10 16:45:12 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -4,7 +4,21 @@ require 'pry'
 def start_game(player1, player2)
   "Hello #{player1} & #{playar2}"
 end
+binding.pry
 
 def play_game(player2)
   "#{player1} is better than #{player2}"
 end
+
+=begin describe "method_man" do
+  it "`start_game` should return greetings for each players name" do
+    expect(start_game("Rocky", "Bullwinkle")).to eq("Hello Rocky & Bullwinkle")
+  end
+end
+
+describe "method_man" do
+  it "`play_game` should return a string that states which player is better" do
+    expect(play_game("Jay-Z", "Beyonce")).to eq("Jay-Z is better than Beyonce")
+  end
+end
+=end
